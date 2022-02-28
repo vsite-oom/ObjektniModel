99207b02b66750e9532e0747aee3d469cea29089
Julijan Sribar <julijan.sribar@gmail.com>
2022-02-28 17:21:46 +0100

    Add code for evaluation of intersection of two lines

--- Apstrakcija/Program.cs
+++ Apstrakcija/Program.cs
@@ -10,6 +10,26 @@ namespace Apstrakcija
     {
         static void Main(string[] args)
         {
+            try
+            {
+                double k1 = double.Parse(args[0]);
+                double l1 = double.Parse(args[1]);
+                double k2 = double.Parse(args[2]);
+                double l2 = double.Parse(args[3]);
+
+                if (k1 == k2)
+                    Console.WriteLine("Lines are parallel");
+                else
+                {
+                    double x = (l2 - l1) / (k1 - k2);
+                    double y = k1 * x + l1;
+                    Console.WriteLine($"Intersection: ({x}, {y})");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid entry");
+            }
         }
     }
 }
