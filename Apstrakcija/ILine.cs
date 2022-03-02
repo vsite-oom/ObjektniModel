a1aff52258fc36cd95e6424876b15f9c7b7af5a5
JUrica11 <jutopolov@vsite.hr>
2022-03-02 20:47:11 +0100

    Implement Line Class

--- Apstrakcija/ILine.cs
+++ Apstrakcija/ILine.cs
@@ -10,16 +10,24 @@ namespace Apstrakcija
     {
         public Point(double x, double y)
         {
-            X = x;
-            Y = y;
+           X = x;
+           Y = y;
 
         }
-        public readonly x;
-        public readonly y;
+        public readonly double X;
+        public readonly double Y;
+
+        public override string ToString()
+        {
+            return$"({X}, {Y})";
+        }
     }
 
    public interface ILine
     {
         Point? Intersection(ILine other);
+
+        double K { get; }
+        double L { get; }
     }
 }
