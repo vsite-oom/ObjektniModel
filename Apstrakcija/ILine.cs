51a534bea1b23518b441ed2916bdf21385df192c
Josip Romac <josipromac@gmail.com>
2022-03-02 20:25:37 +0100

    Create line abstraction

---
+++ Apstrakcija/ILine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apstrakcija
+{
+    public struct Point
+    {
+        public readonly double X, Y;
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    public interface ILine
+    {
+        Point? Intersection(ILine other);
+    }
+}
