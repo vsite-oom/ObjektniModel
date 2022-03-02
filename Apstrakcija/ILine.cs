3b9d712a76f1aa7889c2a9f3d0992284234adae8
JUrica11 <jutopolov@vsite.hr>
2022-03-02 20:26:49 +0100

    Create Line Apstraction

---
+++ Apstrakcija/ILine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apstrakcija
+{
+    public struct Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+
+        }
+        public readonly x;
+        public readonly y;
+    }
+
+   public interface ILine
+    {
+        Point? Intersection(ILine other);
+    }
+}
