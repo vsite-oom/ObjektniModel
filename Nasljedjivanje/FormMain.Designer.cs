cb3394bf1cdf2cea1dfaf48e9c3d7701920454cd
Kristijan <Kristijan@DESKTOP-G5S3BKQ>
2022-03-09 21:21:35 +0100

    Create textboxDefault control

--- Nasljedjivanje/FormMain.Designer.cs
+++ Nasljedjivanje/FormMain.Designer.cs
@@ -28,20 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textboxdefault1 = new Nasljeđivanje.textboxdefault();
+            this.textBox2 = new Nasljeđivanje.textboxdefault();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textboxdefault1
+            // 
+            this.textboxdefault1.Location = new System.Drawing.Point(12, 68);
+            this.textboxdefault1.Name = "textboxdefault1";
+            this.textboxdefault1.Size = new System.Drawing.Size(100, 22);
+            this.textboxdefault1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textboxdefault1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormMain";
             this.Text = "Main Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private textboxdefault textboxdefault1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
