611b9b5c5d8b837253f8e02337b84c8d2c042678
brgrubesa <b.grubesa2@gmailcom>
2022-03-09 21:21:33 +0100

    Create TextBoxDefault control

---
+++ Nasljedjivanje/TextBoxDefault.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nasljeđivanje
+{
+    class TextBoxDefault : TextBox
+    {
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (TextLength == 0)
+                DrawDefaultText(e.Graphics);
+        }
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+            Invalidate();
+        }
+        private void DrawDefaultText(Graphics g)
+        {
+            TextRenderer.DrawText(g, defaultText, Font, ClientRectangle, SystemColors.GrayText, TextFormatFlags.Left);
+        }
+        private string defaultText = "Default";
+    }
+}
