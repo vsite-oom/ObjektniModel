ceb99a4b3435738728bda2ecd7ab1897f68a8e2c
Matteo Madirazza <mamadira@vsite.hr>
2022-03-09 21:30:30 +0100

    TextBox default

--- Nasljedjivanje/FormMain.Designer.cs
+++ Nasljedjivanje/FormMain.Designer.cs
@@ -28,20 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new Nasljeđivanje.TextBoxDefault();
+            this.textBoxDefault1 = new Nasljeđivanje.TextBoxDefault();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(53, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBoxDefault1
+            // 
+            this.textBoxDefault1.Location = new System.Drawing.Point(209, 326);
+            this.textBoxDefault1.Name = "textBoxDefault1";
+            this.textBoxDefault1.Size = new System.Drawing.Size(100, 31);
+            this.textBoxDefault1.TabIndex = 2;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.textBoxDefault1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FormMain";
             this.Text = "Main Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private Nasljeđivanje.TextBoxDefault textBox2;
+        private TextBoxDefault textBoxDefault1;
     }
 }
 
---
+++ Nasljedjivanje/TextBoxDefault.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Nasljeđivanje
+{
+    class TextBoxDefault : TextBox
+    {
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (TextLength == 0)
+                DrawDefaultText(e.Graphics);
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+            Invalidate();
+        }
+
+        private void DrawDefaultText(Graphics g)
+        {
+            TextRenderer.DrawText(g, defaultText, Font, ClientRectangle, SystemColors.GrayText, TextFormatFlags.Left);
+        }
+
+        private string defaultText = "Default";
+
+    }
+}
