29c873da42b6d56b8f04631e5ee1bb4a8311e6c1
Julijan Šribar <julijan.sribar@gmail.com>
2022-03-22 21:22:20 +0100

    Prepare polymorphism sample

--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -10,6 +10,14 @@ namespace Polimorfizam
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            StoryTeller st = new StoryTeller();
+            st.WriteHeading1("Priče iz davnine");
+            st.WriteHeading2("Šuma Striborova");
+            st.WriteParagraph("Zašao neki momak u šumu Striborovu, a nije znao da je ono šuma začarana i da se u njoj svakojaka čuda zbivaju.");
+
+            Console.ReadKey(false);
         }
     }
 }
---
+++ Polimorfizam/StoryTeller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam
+{
+    class StoryTeller
+    {
+        public void WriteParagraph(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void WriteHeading1(string caption)
+        {
+            Console.WriteLine(caption);
+            Console.WriteLine(new string('=', caption.Length));
+        }
+
+        public void WriteHeading2(string caption)
+        {
+            Console.WriteLine(caption);
+            Console.WriteLine(new string('-', caption.Length));
+        }
+    }
+}
