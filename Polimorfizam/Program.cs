6eafe2520c9f513ec61ac0a61b434efd391355b4
OgnjenBillyPOS <ognjen.adamovic@billy.hr>
2022-03-30 20:01:58 +0200

    Adding Interface formater with plain text and html choice (user input)

--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -12,7 +12,23 @@ namespace Polimorfizam
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            StoryTeller st = new StoryTeller();
+            IFormatWriter writer = null;
+            Console.WriteLine("Select format of output: (P)lain or (H)tml");
+            string responseType = Console.ReadLine();
+            switch (responseType.ToLower()[0])
+            {
+                case 'p':
+                    writer = new PlainTextWriter();
+                    break;
+                case 'h':
+                    writer = new HtmlWriter();
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+
+            StoryTeller st = new StoryTeller(writer);
             st.WriteHeading1("Priče iz davnine");
             st.WriteHeading2("Šuma Striborova");
             st.WriteParagraph("Zašao neki momak u šumu Striborovu, a nije znao da je ono šuma začarana i da se u njoj svakojaka čuda zbivaju.");
