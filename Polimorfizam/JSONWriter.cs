64e6b1fae8a0ea8d3d152aa6a04df620ce0a816a
robilovric <robilovric7@gmail.com>
2022-04-04 14:29:04 +0200

    Resolved issue, implemented JSONWriter

---
+++ Polimorfizam/JSONWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam
+{
+    class JSONWriter : IFormatWriter
+    {
+        public void WriteParagraph(string text)
+        {
+            Console.WriteLine($"{{\"paragraph\":\"{text}\"}}");
+        }
+
+        public void WriteHeading1(string caption)
+        {
+            Console.WriteLine($"{{\"heading1\":\"{caption}\"}}");
+            Console.WriteLine(new string('=', caption.Length*2));
+        }
+
+        public void WriteHeading2(string caption)
+        {
+            Console.WriteLine($"{{\"heading2\":\"{caption}\"}}");
+            Console.WriteLine(new string('-', caption.Length*2));
+        }
+    }
+}
--- Polimorfizam/Program.cs
+++ Polimorfizam/Program.cs
@@ -21,7 +21,7 @@ namespace Polimorfizam
             Console.OutputEncoding = Encoding.UTF8;
 
             IFormatWriter writer = null;
-            Console.WriteLine("Select format of output: (P)lain or (H)tml");
+            Console.WriteLine("Select format of output: (P)lain or (H)tml or (J)SON");
             String response = Console.ReadLine();
             switch (response.ToLower()[0])
             {
@@ -31,12 +31,15 @@ namespace Polimorfizam
                 case 'h':
                     writer = new HtmlWriter();
                     break;
+                case 'j':
+                    writer = new JSONWriter();
+                    break;
                 default:
                     Console.WriteLine("Invalid entry");
                     break;
                    
             }
-            StoryTeller st = new StoryTeller(new PlainTextWriter());
+            StoryTeller st = new StoryTeller(writer);
             st.WriteHeading1("Priče iz davnine");
             st.WriteHeading2("Šuma Striborova");
             st.WriteParagraph("Zašao neki momak u šumu Striborovu, a nije znao da je ono šuma začarana i da se u njoj svakojaka čuda zbivaju.");
